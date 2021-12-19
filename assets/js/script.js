6bab19e70fee3fdc65460315b2eac03d30dd2d7c
Mike <mikej.miller440@gmail.com>
2021-12-18 20:46:27 -0600

    pseudo coded the JS file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// TODO: Display date at the top of the calendar
+    // use moment.js to display time in correct format
+    // refresh page every 20 or so min
+
+// TODO: save text input into local storage 
+    // recognize text and save it with the button on the right
+
+// TODO: color code time intervals based on current time
+    // css has the proper stylings
+    // moment.js functionality?
