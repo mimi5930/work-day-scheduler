192443d20ffe820ff007ce9c4f92e35580fe0f1f
Mike <mikej.miller440@gmail.com>
2021-12-21 09:12:08 -0600

    added button click listener with JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 // TODO: Display date at the top of the calendar
     // use moment.js to display time in correct format
     // refresh page every 20 or so min
+    // Ex: Thursday, September 5th
 
 // TODO: save text input into local storage 
     // recognize text and save it with the button on the right
+$(".saveBtn").on("click", function() {
+    btnId = $(this).attr("data-id");
+    console.log(btnId);
+});
+
 
 // TODO: color code time intervals based on current time
     // css has the proper stylings
