486652b98f8ac5a1d14afe8e9cdb3f5dc4d277fe
Mike <mikej.miller440@gmail.com>
2021-12-22 07:59:36 -0600

    added ability to save textarea input into localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,40 @@
+var events = {};
+
 // TODO: Display date at the top of the calendar
     // use moment.js to display time in correct format
     // refresh page every 20 or so min
     // Ex: Thursday, September 5th
 
-// TODO: save text input into local storage 
-    // recognize text and save it with the button on the right
 $(".saveBtn").on("click", function() {
     // retrieve button's data-id value
     var btnId = $(this).attr("data-id");
-    console.log(btnId);
+    
     // match with textarea's data-id value
-    var textareaId = "#task-input-" + btnId;
+    var textareaId = "#event-input-" + btnId;
     var saveText = $(textareaId).val();
-    console.log(saveText);
+    
+    // save event text into array
+    var array = [];
+    if (saveText === "") {
+        array.push({
+            text: ""
+        })
+    }
+    else {
+        array.push({
+            text: saveText
+        })
+    }
+    // update array on tasks object
+    events[btnId] = array;
+    
+    // save into local storage
+    saveEvents();
 });
 
-
+var saveEvents = function() {
+    localStorage.setItem("events", JSON.stringify(events));
+};
 // TODO: color code time intervals based on current time
     // css has the proper stylings
     // moment.js functionality?
