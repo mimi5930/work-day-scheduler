8fdd1b96c05fe8d60ab88d0359f9bdf60bd6a199
Mike <mikej.miller440@gmail.com>
2021-12-22 10:48:44 -0600

    added a load function to grab files from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,13 @@ $(".saveBtn").on("click", function() {
     var array = [];
     if (saveText === "") {
         array.push({
+            id: btnId,
             text: ""
         })
     }
     else {
         array.push({
+            id: btnId,
             text: saveText
         })
     }
@@ -35,6 +37,28 @@ $(".saveBtn").on("click", function() {
 var saveEvents = function() {
     localStorage.setItem("events", JSON.stringify(events));
 };
+
+var loadEvents = function() {
+    events = JSON.parse(localStorage.getItem("events"));
+
+    // create empty events object if nothing's in local storage
+    if (!events) {
+        events = {};
+    }
+
+    // loop through and fill textareas with values
+    for (i = 0; i < events.length; i++ ) {
+        var eventId = "#event-input-" + events[i][0].id;
+        var eventText = events[i][0].text;
+        $(eventId).val(eventText);
+        console.log(eventId);
+        console.log(eventText);
+    }
+}
+
 // TODO: color code time intervals based on current time
     // css has the proper stylings
     // moment.js functionality?
+
+// load tasks on page startup
+loadEvents();
